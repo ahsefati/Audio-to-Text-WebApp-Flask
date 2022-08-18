e9980e51d9e8db1305ec64b60b4265c0b1142a6a
Amirhossein Sefati <ahsefati77@gmail.com>
2022-08-18 23:40:47 +0000

    Audio to Text Flask-Python

---
+++ flask_app.py
@@ -0,0 +1,90 @@
+from flask import Flask, render_template, request
+
+# importing libraries
+import speech_recognition as sr
+import os
+from pydub import AudioSegment
+from pydub.silence import split_on_silence
+
+
+app = Flask(__name__)
+
+# create a speech recognition object
+r = sr.Recognizer()
+# a function that splits the audio file into chunks
+# and applies speech recognition
+def get_large_audio_transcription(path):
+    """
+    Splitting the large audio file into chunks
+    and apply speech recognition on each of these chunks
+    """
+    # open the audio file using pydub
+    sound = AudioSegment.from_wav(path)
+    # split audio sound where silence is 700 miliseconds or more and get chunks
+    chunks = split_on_silence(sound,
+        # experiment with this value for your target audio file
+        min_silence_len = 500,
+        # adjust this per requirement
+        silence_thresh = sound.dBFS-14,
+        # keep the silence for 1 second, adjustable as well
+        keep_silence=500,
+    )
+    folder_name = "audio"
+    # create a directory to store the audio chunks
+    if not os.path.isdir(folder_name):
+        os.mkdir(folder_name)
+    whole_text = ""
+    # process each chunk
+    for i, audio_chunk in enumerate(chunks, start=1):
+        # export audio chunk and save it in
+        # the `folder_name` directory.
+        chunk_filename = os.path.join(folder_name, f"chunk{i}.wav")
+        audio_chunk.export(chunk_filename, format="wav")
+        # recognize the chunk
+        with sr.AudioFile(chunk_filename) as source:
+            audio_listened = r.record(source)
+            # try converting it to text
+            try:
+                text = r.recognize_google(audio_listened)
+            except sr.UnknownValueError as e:
+                print("Error:", str(e))
+            else:
+                text = f"{text.capitalize()}. "
+                # print(text)
+                whole_text += text
+    # return the text for all chunks detected
+    return whole_text
+
+
+def convertToWav(path):
+    sound = AudioSegment.from_mp3(path)
+    sound.export("./newfile.wav", format="wav")
+    result = get_large_audio_transcription("./newfile.wav")
+    return result
+
+@app.route('/')
+def hello_world():
+    # result = convertToWav("./mysite/assets/test2.mp3")
+    # directory = os.getcwd()
+    # print(result)
+    return render_template("index.html")
+
+
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file():
+   if request.method == 'POST':
+      f = request.files['file']
+      f.save("./mysite/assets/newtestcase.mp3")
+      return render_template("uploaded.html")
+
+
+
+@app.route("/audio2text", methods=['GET'])
+def audiototext():
+    result = convertToWav("./mysite/assets/newtestcase.mp3")
+    return result
+
+
+
+
+
